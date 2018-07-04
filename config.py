b90a5fff06bb54ceb00cff60bc0b0448220e69b4
MadMrCrazy <madmrcrazy@gmail.com>
2018-07-04 23:01:32 +0930

    Added config settings
    Signed-off-by: MadMrCrazy <madmrcrazy@gmail.com>

---
+++ config.py
@@ -0,0 +1,4 @@
+'''
+Configuration File for CraeBot v0.2.1-v0.2.2
+'''
+token = ""
--- run.py
+++ run.py
@@ -1,5 +1,10 @@
 print("Running discord bot")
 token = "token input"
-import main
-bot = main.crae
-bot.run(token)
+import config
+if config.token == "":
+    print("Initialisation error, Insert bot token in config.py")
+else:
+    print("Config Imported!")
+    import main
+    bot = main.crae
+    bot.run(config.token)
